bd6ae4605002c9c9b78a7d7724c148a9cee01e79
Yarik23232 <yarik232yarik232@gmail.com>
2025-03-21 17:14:35 +0300

    Добавили продукту детали

--- config/shop/urls.py
+++ config/shop/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import catalog, orders, order_create
+from .views import catalog, orders, order_create, product_detail
 
 app_name = "shop"
 
 urlpatterns = [
     path("", catalog, name="catalog"),
     path("orders", orders, name="orders"),
-    path("order_create", order_create, name="order_create")
+    path("order_create", order_create, name="order_create"),
+    path("product_detail/<int:product_id>", product_detail, name="product_detail")
 ]
--- config/shop/views.py
+++ config/shop/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Product, Order
 
+def index(request):
+    return render(request, "shop/index.html")
+
 def catalog(request):
     product_list = Product.objects.all()
     return render(request, "shop/catalog.html", {"products":product_list})
@@ -11,3 +14,8 @@ def orders(request):
 
 def order_create(request):
     return render(request, "shop/order_create.html")
+
+def product_detail(request, product_id):
+    product_in_bd = Product.objects.get(id=product_id)
+    return render(request, "shop/product_detail.html", {"product": product_in_bd})
+
